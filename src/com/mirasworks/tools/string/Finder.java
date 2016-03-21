85b49bbb7068d2ad415a181c4640d5b55cc9793f
DamienMiras <damien.miras@gmail.com>
2016-03-21 15:34:52 +0100

    minor add lib

--- src/com/mirasworks/tools/string/Finder.java
+++ src/com/mirasworks/tools/string/Finder.java
@@ -3,27 +3,38 @@ package com.mirasworks.tools.string;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class Finder {
 
-	/**
-	 *  extract text between two pattern , it is  thread Safe
-	 * @param pattern1
-	 * @param pattern2
-	 * @param inputText
-	 * @return
-	 */
-	public String extractFirst(String pattern1, String pattern2, String inputText) {
+    /**
+     * extract text between two pattern
+     *
+     * @param strStart
+     * @param strEnd
+     * @param inputText
+     * @return
+     */
+    public static String extractFirst(String strStart, String strEnd, String inputText) {
 
-		String regexString = Pattern.quote(pattern1) + "(.*?)" + Pattern.quote(pattern2);
-		Pattern pattern = Pattern.compile(regexString);
-	
-		Matcher matcher = pattern.matcher(inputText);
+        String regexString = Pattern.quote(strStart) + "(.*?)" + Pattern.quote(strEnd);
+        Pattern pattern = Pattern.compile(regexString);
 
-		while (matcher.find()) {
-			return  matcher.group(1);
-		}
-		return "";
-	}
+        Matcher matcher = pattern.matcher(inputText);
+
+        while (matcher.find()) {
+            return matcher.group(1);
+        }
+        return "";
+    }
+
+    public static String replaceBetween(String inputText, String strStart, String replacement, String strEnd) {
+
+        String regexString = Pattern.quote(strStart) + "(.*?)" + Pattern.quote(strEnd);
+        return inputText.replaceAll(regexString, replacement);
+
+    }
+
+    public static String replaceNumbersBy(String inputText, String replacement) {
+        return inputText.replaceAll("-?\\d+", replacement);
+    }
 
 }
